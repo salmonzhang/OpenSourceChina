f43d54483ff42303f44ed41cdc26dd72392cccc9
salmonzhang <ahzhangxuping@163.com>
2017-08-08 20:52:37 +0800

    底部fragmentTabHost展示完成

--- app/src/main/java/com/example/opensourcechina/activity/MainActivity.java
+++ app/src/main/java/com/example/opensourcechina/activity/MainActivity.java
@@ -6,13 +6,17 @@ import android.support.v4.app.FragmentTabHost;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
 import android.widget.TabHost;
+import android.widget.TextView;
 
 import com.example.opensourcechina.R;
-import com.example.opensourcechina.fragment.NewsFragment;
+import com.example.opensourcechina.bean.MainTabs;
+import com.example.opensourcechina.utils.ToastUtil;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -31,16 +35,38 @@ public class MainActivity extends AppCompatActivity {
 
     private void init() {
         //绑定
-        mFtabMainBottomLayout.setup(this,getSupportFragmentManager(),R.id.fl_main_show_layout);
+        mFtabMainBottomLayout.setup(this, getSupportFragmentManager(), R.id.fl_main_show_layout);
 
-        for (int i = 0; i < 5; i++) {
+        MainTabs[] mainTabses = MainTabs.values();
+        for (int i = 0; i < mainTabses.length; i++) {
             //添加页签
-            TabHost.TabSpec tabSpec = mFtabMainBottomLayout.newTabSpec("tag");//复用
+            TabHost.TabSpec tabSpec = mFtabMainBottomLayout.newTabSpec(mainTabses[i].mTag);//复用
             //添加指示器
             View view = View.inflate(this, R.layout.tab_indicator, null);
+
+            //标题
+            TextView title = (TextView) view.findViewById(R.id.tab_title);
+            title.setText(mainTabses[i].mText);
+
+            //图片
+            ImageView iv = (ImageView) view.findViewById(R.id.iv_icon);
+            iv.setImageResource(mainTabses[i].mIv);
+
+            if (i == 2) {
+                iv.setVisibility(View.INVISIBLE);
+            }
+
             tabSpec.setIndicator(view);
-            Class<? extends Fragment> clss = NewsFragment.class;
+            Class<? extends Fragment> clss = mainTabses[i].mClass;
             mFtabMainBottomLayout.addTab(tabSpec, clss, null);
+
+            //去掉分隔线
+            mFtabMainBottomLayout.getTabWidget().setDividerDrawable(null);
         }
+    }
+
+    @OnClick(R.id.iv_icon_selector)
+    public void onClick(View view) {
+        ToastUtil.showToast("弹一弹被点击了");
     }
 }
