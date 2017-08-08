d349e27d59cece3dabd8fe77773f4dbfd05e7ebb
salmonzhang <ahzhangxuping@163.com>
2017-08-08 12:37:33 +0800

    导入解析emoji表情类

---
+++ app/src/main/java/com/example/opensourcechina/view/emoji/EmojiKeyboardFragment.java
@@ -0,0 +1,21 @@
+package com.example.opensourcechina.view.emoji;
+
+import android.view.View;
+
+import com.example.opensourcechina.fragment.BaseFragment;
+
+
+//TODO:后期完成表情
+public class EmojiKeyboardFragment extends BaseFragment {
+
+
+    @Override
+    protected View getSuccessView() {
+        return null;
+    }
+
+    @Override
+    protected Object rquestData() {
+        return null;
+    }
+}
